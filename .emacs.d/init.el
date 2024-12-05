d8027f648ad705b737b9a540a0965a7bca561cc1
WooHyoung Jeon <jeon@ptrtoj.com>
2024-12-05 23:35:06 +0900

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,9 @@
 ;; (https://www.gnu.org/software/emacs/manual/html_node/elisp/Comment-Tips.html)
 
 ;;; Code:
+
 ;; [Quoted from the linked document]
+
 ;; Using multiple top-level code sections has the advantage that
 ;; it avoids introducing an additional nesting level but it also means
 ;; that the section named ‘Code’ does not contain all the code, which
@@ -13,8 +15,24 @@
 ;; To avoid that, you should put no code at all inside that section;
 ;; that way it can be considered a separator instead of a section heading.
 
-;;; Defaults
-;;;; Emacs Itself
+;; [Dependencies]
+
+;; for Flyspell
+;; - aspell (or hunspell)
+;; - And also dictionary files if you chose to use hunspell
+;; - '*.aff' && '*.dic' (e.g. 'en_US.aff' and 'en_US.dic')
+;;
+;; Dictionary files should be placed in '/USER_HOME_DIR/Library/Spelling/' or '/Library/Spelling/'.
+;;
+;; Homebrew itself doesn't provide dictionaries for Hunspell,
+;; but you can download compatible dictionaries from other sources,
+;; such as, from here (https://cgit.freedesktop.org/libreoffice/dictionaries/tree/).
+;;
+;;   $ cd ~/Library/Spelling/
+;;   $ wget https://github.com/ropensci/hunspell/raw/master/inst/dict/en_US.aff
+;;   $ wget https://github.com/ropensci/hunspell/raw/master/inst/dict/en_US.dic
+
+;;; Emacs Itself
 (use-package emacs
   :custom
   (user-full-name "WooHyoung Jeon")
@@ -23,20 +41,20 @@
   (inhibit-startup-screen t)
   (visible-bell t))
 
-;;;; Prevent Emacs Generating 'custom.el'
+;;; Prevent Emacs Generating 'custom.el'
 (use-package cus-edit
   :custom
   (custom-file (concat user-emacs-directory "custom.el"))
   (when (file-exists-p custom-file)
     (load custom-file)))
 
-;;;; Prevent Emacs Generating Backup Files
+;;; Prevent Emacs Generating Backup Files
 ;; Dropped. Backup files are actually helpful.
 ;; (use-package files
 ;;   :custom
 ;;   (make-backup-files nil))
 
-;;;; Remember Recent Files
+;;; Remember Recent Files
 ;; Dropped. It isn't hard to open wanted file, and also there's 'Dired'.
 ;; 'C-x C-r' was 'find-file-read-only'
 ;; (use-package recentf
@@ -45,28 +63,28 @@
 ;;   :config
 ;;   (recentf-mode 1))
 
-;;;; Remember Recent Command History
+;;; Remember Recent Command History
 (use-package savehist
   :config
   (savehist-mode 1))
 
-;;;; Remember Last Cursor Postion from File
+;;; Remember Last Cursor Postion from File
 (use-package saveplace
   :config
   (save-place-mode 1))
 
-;;;; Delete Selection as I Type
+;;; Delete Selection as I Type
 (use-package delsel
   :config
   (delete-selection-mode 1))
 
-;;;; Automatically Match Parenthesis
+;;; Automatically Match Parenthesis
 (use-package elec-pair
   :hook
   (prog-mode . electric-pair-mode)
   (org-mode . electric-pair-mode))
 
-;;;; Clean Whitespaces
+;;; Clean Whitespaces
 (use-package whitespace
   ;; show whitespace with colors
   ;;:custom
@@ -76,29 +94,19 @@
   ;;(prog-mode . whitespace-mode)
   (before-save . whitespace-cleanup))
 
-;;;; Revert Buffer Automatically
+;;; Revert Buffer Automatically
 ;; Dropped. Don't think I *REALLY* need this feature, yet.
-;; (use-package autorevert
-;;     :diminish (auto-revert-mode)
-;;     :config
-;;     (global-auto-revert-mode t))
+;; However, I want to diminish the minor mode section.
+(use-package autorevert
+  :diminish (auto-revert-mode))
+  ;; :config
+  ;; (global-auto-revert-mode t))
 
-;;;; Spellcheck
-;; [Requirements]
-;; - aspell (or hunspell)
-;; - And also dictionary files if you chose to use hunspell
-;; - '*.aff' && '*.dic' (e.g. 'en_US.aff' and 'en_US.dic')
+;;; Provide Documents
+(use-package eldoc
+  :diminish)
 
-;; Dictionary files should be placed in '/USER_HOME_DIR/Library/Spelling/' or '/Library/Spelling/'.
-
-;; Homebrew itself doesn't provide dictionaries for Hunspell,
-;; but you can download compatible dictionaries from other sources,
-;; such as, from here (https://cgit.freedesktop.org/libreoffice/dictionaries/tree/).
-
-;;   $ cd ~/Library/Spelling/
-;;   $ wget https://github.com/ropensci/hunspell/raw/master/inst/dict/en_US.aff
-;;   $ wget https://github.com/ropensci/hunspell/raw/master/inst/dict/en_US.dic
-
+;;; Spellcheck
 (use-package flyspell
   :diminish
   ;;:config
@@ -109,18 +117,18 @@
   (prog-mode . flyspell-prog-mode))
 ;;(org-mode . flyspell-prog-mode))
 
-;;;; UI
-;;;;; Disable Tool Bar
+;;; Disable Tool Bar
 (use-package tool-bar
   :config
   (tool-bar-mode -1))
 
-;;;;; Disable Scroll Bar
-(use-package scroll-bar
-  :config
-  (scroll-bar-mode 0))
+;;; Disable Scroll Bar
+;; I found out that actually I use scrollbar, too.
+;; (use-package scroll-bar
+;;   :config
+;;   (scroll-bar-mode 0))
 
-;;;;; Fonts and Transparent Frames
+;;; Fonts and Transparent Frames
 (use-package faces
   :config
   (set-face-attribute 'font-lock-keyword-face nil :weight 'bold)
@@ -141,11 +149,11 @@
   (set-frame-parameter (selected-frame) 'alpha '(95 95))
   (add-to-list 'default-frame-alist '(alpha 95 95)))
 
-;; [DO NOT WORK] Blur the background
+;; [DOES NOT WORK] Blur the background
 ;;(set-face-background 'default "mac:windowBackgroundColor")
 ;;(set-face-stipple 'default "alpha:30%"))
 
-;;;;; Show Line Numbers
+;;; Show Line Numbers
 (use-package display-line-numbers
   :custom
   (display-line-numbers-type 'relative)
@@ -160,30 +168,29 @@
   ;; prog-mode doesn't enable org-mode in this mode
   (org-mode . display-line-numbers-mode))
 
-;;;; Show Column Number
+;; Show Column Number
 (use-package simple
   :config
   (column-number-mode 1))
 
-;;;;; Show Max Column Indicator
+;;; Show Max Column Indicator
 (use-package display-fill-column-indicator
   :hook
   (prog-mode . display-fill-column-indicator-mode)
   (org-mode . display-fill-column-indicator-mode))
 
-;;;;; Highlight Current Cursor Line
+;;; Highlight Current Cursor Line
 (use-package hl-line
   :config
   (global-hl-line-mode 1))
 
-;;;;; Show URLs as a Clickable Link
+;;; Show URLs as a Clickable Link
 (use-package goto-addr
   :hook
   (prog-mode . goto-address-prog-mode)
   (text-mode . goto-address-prog-mode))
 
-;;; 3rd Party Plugins
-;;;; Set up Package Manager
+;;; Set up Package Manager
 ;; Moved 'package-refresh-contents' to 'auto-package-update' config section
 (use-package package
   :config
@@ -192,7 +199,7 @@
   (unless package-archive-contents
     (package-refresh-contents)))
 
-;;;; Automatically Update Packages
+;;; Automatically Update Packages
 (use-package auto-package-update
   :ensure t
   :bind
