5238fd19d78f139130c5b36ad59b4c71b6cd966d
WooHyoung Jeon <jeon@ptrtoj.com>
2025-02-01 21:24:34 +0900

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,24 +1,12 @@
-;; Testing without below. [2024-12-27 23:27]
-;; See: https://github.com/hrs/dotfiles/blob/main/emacs/.config/emacs/init.el
-;; 'Robin Schwartz' added below but it seems I do not need these.
-; Configure package.el to include MELPA.
-;(require 'package)
-;(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
-;(package-initialize)
-
-;(defun package--save-selected-packages (&rest opt) nil)
-
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 
-;; Also, if you're using basic ~init.el~,
-;; you can hide subheadings with below.
-;; And, 'outline-regexp' enables only show 2 levels of headings
-;; See: https://emacs.stackexchange.com/q/60420/40897 from Stack Exchange
-  ;; Local Variables:
-  ;; outline-minor-mode-cycle: t
-  ;; outline-regexp: ";\\{3,4\\} "
-  ;; eval: (outline-minor-mode)
-  ;; eval: (outline-hide-body)
-  ;; End:
+;; Also, if you're using basic ~init.el~, you can hide subheadings with below.
+;; 'outline-regexp' enables only showing 2 levels of headings.
+;; See: https://emacs.stackexchange.com/q/60420/40897.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Local Variables:
+;; outline-minor-mode-cycle: t
+;; outline-regexp: ";\\{3,4\\} "
+;; eval: (outline-minor-mode)
+;; eval: (outline-hide-body)
+;; End:
--- .zshrc
+++ .zshrc
@@ -1,7 +1,12 @@
 # PATH
-export PATH="/Users/jeon/.local/bin:$PATH"
-export LIBRARY_PATH="/Users/jeon/.local/lib:$LIBRARY_PATH"
-export C_INCLUDE_PATH="/Users/jeon/.local/include:$C_INCLUDE_PATH"
+# (Link to the intersting discussion about where to put which var: 
+# https://unix.stackexchange.com/a/71258)
+export PATH="/Users/jeon/.local/bin${PATH:+:$PATH}"
+export CPATH="/Users/jeon/.local/bin${CPATH:+:$CPATH}"
+export LIBRARY_PATH="/Users/jeon/.local/lib${LIBRARY_PATH:+:$LIBRARY_PATH}"
+export LD_LIBRARY_PATH="/Users/jeon/.local/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export C_INCLUDE_PATH="/Users/jeon/.local/include${C_INCLUDE_PATH:+:$C_INCLUDE_PATH}"
+export CPLUS_INCLUDE_PATH="/Users/jeon/.local/include${CPLUS_INCLUDE_PATH:+:$CPLUS_INCLUDE_PATH}"
 
 # GPG
 export GPG_TTY=$(tty)
